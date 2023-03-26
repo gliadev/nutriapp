988da42151b7f32bc57d2872d23de21bf46ed49b
Adolfo <gliatem@gmail.com>
2023-03-27 00:25:34 +0200

    modificado1

--- server/index.js
+++ server/index.js
@@ -29,22 +29,22 @@ db.on("error", console.error.bind(console, "Error en la conexión a MongoDB:"));
 db.once("open", function() {
   console.log("Conexión a la base de datos establecida");
 
+  // Crear la aplicación Express
+  const app = express();
+
+  // Configurar middlewares
+  app.use(express.json());
+
   // Agregar las rutas de las colecciones
   app.use("/api/paciente", pacienteRoutes);
   app.use("/api/entrenamiento", entrenamientoRoutes);
   app.use("/api/nutricionista", nutricionistaRoutes);
   app.use("/api/cita", citaRoutes);
   app.use("/api/menus-semanal", menusSemanalRoutes);
-});
 
-// Crear la aplicación Express
-const app = express();
-
-// Configurar middlewares
-app.use(express.json());
-
-// Iniciar el servidor
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Servidor ejecutándose en el puerto ${PORT}`);
+  // Iniciar el servidor
+  const PORT = process.env.PORT || 3000;
+  app.listen(PORT, () => {
+    console.log(`Servidor ejecutándose en el puerto ${PORT}`);
+  });
 });
