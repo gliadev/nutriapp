1706c8bc9f8290c6a96dc08901409fc84f3038c1
Adolfo <gliatem@gmail.com>
2023-04-02 20:21:19 +0200

    modificando un poco la home

--- nutriapp/src/components/Wave.js
+++ nutriapp/src/components/Wave.js
@@ -1,59 +1,23 @@
-import React, { useRef, useEffect } from "react";
+import React from "react";
+require("../styles/Wave.css");
 
-const Wave = () => {
-  const canvasRef = useRef(null);
-
-  const drawWave = (canvas, context, position, phase, waveHeightFactor) => {
-    const width = canvas.width;
-    const height = canvas.height;
-    const waveHeight = (height / 4) * waveHeightFactor;
-
-    context.beginPath();
-    context.moveTo(0, position);
-    context.quadraticCurveTo(
-      width / 4,
-      position - waveHeight + Math.sin(phase) * waveHeight,
-      width / 2,
-      position
-    );
-    context.quadraticCurveTo(
-      (3 * width) / 4,
-      position + waveHeight + Math.sin(phase + Math.PI) * waveHeight,
-      width,
-      position
-    );
-    context.lineTo(width, height);
-    context.lineTo(0, height);
-    context.closePath();
-    context.fillStyle = "#f8b24e";
-    context.fill();
-  };
-
-  const randomBetween = (min, max) => {
-    return Math.random() * (max - min) + min;
-  };
-
-  useEffect(() => {
-    const canvas = canvasRef.current;
-    const context = canvas.getContext("2d");
-
-    const position = canvas.height / 2;
-    const waveHeightFactor = randomBetween(0.2, 1);
-
-    let phase = 0;
-    const waveSpeed = 0.015;
-
-    const animateWave = () => {
-      context.clearRect(0, 0, canvas.width, canvas.height);
-      drawWave(canvas, context, position, phase, waveHeightFactor);
-      phase += waveSpeed;
-      requestAnimationFrame(animateWave);
-    };
-
-    animateWave();
-  }, []);
-
-  return <canvas ref={canvasRef} />;
-};
+function Wave() {
+  return (
+    <div className="wave-container">
+      <svg
+        className="wave-svg"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 1440 320"
+      >
+        <path
+          fill="#f8b24e"
+          fillOpacity="1"
+          d="M0,192L48,202.7C96,213,192,235,288,213.3C384,192,480,128,576,106.7C672,85,768,107,864,117.3C960,128,1056,128,1152,133.3C1248,139,1344,149,1392,154.7L1440,160L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"
+        ></path>
+      </svg>
+      <canvas className="wave-canvas"></canvas>
+    </div>
+  );
+}
 
 export default Wave;
